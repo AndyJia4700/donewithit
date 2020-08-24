394b1cbb362e5d974107b5928bda8634e7b0a6a6
andy <jia470079834@gmail.com>
2020-08-23 22:15:55 -0400

    native init commit

--- App.js
+++ App.js
@@ -42,20 +42,23 @@ export default function App() {
           height: landscape ? "100%" : "30%",
           flex: 1,
           flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
         <View
           style={{
             backgroundColor: "black",
             width: 100,
-            height: 100,
+            height: 300,
+            alignSelf: "flex-start",
           }}
         />
         <View
           style={{
             backgroundColor: "white",
             width: 100,
-            height: 100,
+            height: 200,
           }}
         />
         <View
