87e2a4eafb94b5cea6464c9a669b69d1dec5546b
andy <jia470079834@gmail.com>
2020-08-29 23:45:50 -0400

    practice

--- App.js
+++ App.js
@@ -20,8 +20,48 @@ import {
 } from "@react-native-community/hooks";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import AppText from "./app/components/AppText";
 
 export default function App() {
   // return <WelcomeScreen />;
-  return <ViewImageScreen />;
+  // return <ViewImageScreen />;
+  return (
+    // <View
+    //   style={{
+    //     flex: 1,
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //   }}
+    // >
+    //   <View
+    //     style={{
+    //       backgroundColor: "dodgerblue",
+    //       width: 100,
+    //       height: 100,
+    //       // borderWidth: 10,
+    //       // borderColor: "royablue",
+    //       // borderRadius: 50,
+    //       // borderTopWidth: 20,
+    //       // borderTopLeftRadius: 50,
+
+    //       // shadowColor: "red",
+    //       // shadowOffset: { width: 10, height: 10 },
+    //       // shadowOpacity: 1,
+    //       // shadowRadius: 10,
+
+    //       //Android
+    //       // elevation: 20,
+    //     }}
+    //   ></View>
+    // </View>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <AppText>I Love React Native</AppText>
+    </View>
+  );
 }
---
+++ app/components/AppText.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View, Platform } from "react-native";
+// import { Text } from "react-native";
+
+export default function AppText({ children }) {
+  return <Text style={styles.text}>{children}</Text>;
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+  },
+});
