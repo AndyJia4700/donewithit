90fa3d6c8fa9520f6b1d4988d7cc3121789badce
andy <jia470079834@gmail.com>
2020-09-17 23:37:05 -0400

    screen

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Constants from "expo-constants";
 
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
 
 export default function Screen({ children }) {
   return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
